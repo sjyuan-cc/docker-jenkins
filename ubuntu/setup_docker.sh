68f47d3b6dfc3a95e11e0443e28bc4dd77178859
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-11-21 15:00:53 +0800

    update docker installation sh

--- ubuntu/setup_docker.sh
+++ ubuntu/setup_docker.sh
@@ -3,17 +3,13 @@
 sudo apt-get install -y curl
 
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo touch /etc/apt/sources.list.d/docker.list
-sudo chmod o+w /etc/apt/sources.list.d/docker.list
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-sudo chmod o-w /etc/apt/sources.list.d/docker.list
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+
 sudo apt-get update
-sudo apt-get purge lxc-docker
-sudo apt-cache policy docker-engine
-sudo apt-get update
+apt-cache policy docker-engine
 sudo apt-get install -y docker-engine
+
 sudo usermod -aG docker $(whoami)
 
 sudo touch /usr/local/bin/docker-compose
